531d2c637822e73ab68f7f7c72a20ee21c79fefe
tam11a <ibrahimsadiktamim@gmail.com>
2025-07-09 14:33:45 +0600

    added: layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,13 +2,13 @@ import { Stack } from "expo-router";
 import "react-native-reanimated";
 
 import { LinearGradient } from "expo-linear-gradient";
-import { Dimensions, StatusBar, View } from "react-native";
-import Svg, { Circle } from "react-native-svg";
+import { StatusBar, View } from "react-native";
 import "./global.css";
 
 // SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  // Uncomment the following lines if you want to use custom fonts
   // const [loaded, error] = useFonts({
   //   Righteous_400Regular,
   //   Outfit_400Regular,
@@ -25,34 +25,36 @@ export default function RootLayout() {
   //   return null;
   // }
 
+  // Set the status bar style and background color
   StatusBar.setBarStyle("light-content", true);
   StatusBar.setBackgroundColor("transparent", true);
   StatusBar.setTranslucent(true);
 
-  const gap = 10;
-  const dotSize = 2;
-  const { width: screenWidth, height: screenHeight } = Dimensions.get("window");
-  const cols = Math.ceil(screenWidth / gap) + 1;
-  const rows = Math.ceil(screenHeight / gap) + 1;
+  // Set the background color of the app
+  // const gap = 10;
+  // const dotSize = 2;
+  // const { width: screenWidth, height: screenHeight } = Dimensions.get("window");
+  // const cols = Math.ceil((screenWidth * 1.5) / gap) + 1;
+  // const rows = Math.ceil((screenHeight * 1.5) / gap) + 1;
   return (
     <LinearGradient
       colors={["#764985", "#090030"]}
       start={{ x: 0, y: 0 }}
       end={{ x: 1, y: 1 }}
-      style={{ flex: 1 }}
+      style={{ flex: 1, position: "relative" }}
     >
-      <Svg
+      {/* <Svg
         pointerEvents="none"
         style={{
           position: "absolute",
-          width: screenWidth,
-          height: screenHeight,
-          zIndex: 1,
+          width: screenWidth * 1.5,
+          height: screenHeight * 1.5,
+          zIndex: 0,
           top: 0,
           left: 0,
         }}
-        width={screenWidth}
-        height={screenHeight}
+        width={screenWidth * 1.5}
+        height={screenHeight * 1.5}
       >
         {Array.from({ length: rows }).map((_, row) =>
           Array.from({ length: cols }).map((_, col) => (
@@ -61,12 +63,12 @@ export default function RootLayout() {
               cx={col * gap}
               cy={row * gap}
               r={dotSize / 2}
-              fill="rgba(255,255,255,0.10)"
+              fill={"rgba(255,255,255,0.10)"}
             />
           ))
         )}
-      </Svg>
-      <View className="flex-1">
+      </Svg> */}
+      <View className="flex-1 z-1">
         <Stack
           screenOptions={{
             headerShown: false,
